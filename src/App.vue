cf886b1de745c557e009bc126226156dee484dba
Isaiah Weidl <2137144+IWeidl@users.noreply.github.com>
2022-04-27 12:51:56 +1000

    Implemented summoner text field and region drop down box.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <button @click="getSummoner">GET EXLEX</button>
+  <select v-model="region">
+    <option>OC1</option>
+    <option>NA1</option>
+    <option>EUN1</option>
+    <option>EUW1</option>
+    <option>JP1</option>
+    <option>KR</option>
+    <option>BR1</option>
+    <option>TR1</option>
+    <option>RU</option>
+  </select>
+  <input v-model="summoner">
+  <button @click="getSummoner">Get {{ summoner }}</button>
   <pre>{{ results }}</pre>
 </template>
 
@@ -10,12 +22,14 @@ export default {
 
   data() {
     return {
+      region: null,
+      summoner: null,
       results: null
     }
   },
   methods: {
     async getSummoner() {
-      axios.get('http://localhost:3000/oc1/summoner/exlex')
+      axios.get(`http://localhost:3000/${this.region}/summoner/${this.summoner}`)
       .then((response) => {
         this.results = response.data;
       })
